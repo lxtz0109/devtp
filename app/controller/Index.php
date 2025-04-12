bbbde12d0ee847eae95a970de1756c4a18878b0d
wzh333 <wangrander@163.com>
2025-04-12 11:11:18 +0800

    Index第一次提交

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -11,6 +11,7 @@ class Index extends BaseController
         return '<style>*{ padding: 0; margin: 0; }</style><iframe src="https://www.thinkphp.cn/welcome?version=' . \think\facade\App::version() . '" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>';
     }
 
+
     public function hello($name = 'ThinkPHP8')
     {
         return 'hello,' . $name;
